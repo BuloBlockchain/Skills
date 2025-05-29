fa679a3179001fe65d5d2b57e7cfc72cfcac5455
BuloBlockchain <gabrielalvesadm13@gmail.com>
2025-05-29 13:51:52 -0400

    message

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -17,3 +17,9 @@ function checkCountValue() {
     alert("Your Instagram post gained 20 followers! Keep it up!");
   }
 }
+
+function resetCount() {
+  count = 0; // Reset the count to 0
+  displayCount(); // Update the display
+  alert("The follower count has been reset to 0.");
+}
